d850ff0681149bac82e120f2a5580fe36badac06
Sander F. Sandøy <sanderfsandoy@gmail.com>
2024-02-05 14:21:25 +0100

    feat: Sahramor

--- app/page.tsx
+++ app/page.tsx
@@ -1,105 +1,82 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex bg-white  min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/bilde2.jpg"
-              alt="om-arkitektur Logo"
-              width={150}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <main className="flex bg-white  min-h-screen flex-col items-center p-24 md:gap-24 gap-4">
+            <div className="className='grid grid-cols-3 w-full">
+                <div className="flex h-48 w-full items-end justify-start">
+                    <a
+                        className="pointer-events-none flex place-items-center gap-2"
+                        href="/"
+                    >
+                        <Image
+                            src="/bilde2.jpg"
+                            alt="om-arkitektur Logo"
+                            width={150}
+                            height={24}
+                            priority
+                        />
+                    </a>
+                </div>
+            </div>
 
-      <div>
-        <Image
-              src="/bilde1.jpg"
-              alt="om-arkitektur Logo"
-              width={400}
-              height={24}
-              priority
-            />
-      </div>  
-{/*
-      <div className="flex overflow-hidden w-full animate-horizonalMotion">
-        <div>
-      <Image
-              src="/bilde2.jpg"
-              alt="om-arkitektur Logo"
-              width={400}
-              height={24}
-              priority
-            />
-            </div>
-            <div>
+            <div className='grid grid-cols-1 lg:grid-cols-3 w-full gap-8'>
+                <div className='text-black col-start-1'>
+                    <p>
+                        Josefines gate 32, 0351 Oslo
+                    </p>
+                    <p>
+                        <a href="mailto:post@omarkitektur.no">
+                            post@omarkitektur.no
+                        </a>
+                    </p>
+                    <p>
+                        <a href="tel:+ 4790552560">+47 90 55 25 60</a>
+                    </p>
+                    <p>
+                        <a href='https://www.instagram.com/om_arkitektur/'>
+                            instaicon om_arkitektur
+                        </a>
+                    </p>
+                </div>
                 <Image
-              src="/bilde2.jpg"
-              alt="om-arkitektur Logo"
-              width={400}
-              height={24}
-              priority
-            />
+                    className='col-span-1 justify-self-center max-md:my-8'
+                    src="/bilde1.jpg"
+                    alt="om-arkitektur Logo"
+                    width={600}
+                    height={600}
+                    priority
+                />
             </div>
-            <div>
-                <Image
-              src="/bilde2.jpg"
-              alt="om-arkitektur Logo"
-              width={400}
-              height={24}
-              priority
-            />
-            </div>
-            <div>
-                <Image
-              src="/bilde2.jpg"
-              alt="om-arkitektur Logo"
-              width={400}
-              height={24}
-              priority
-            />
-            </div>
-        </div> 
-        */}
-      
-    
-      <div className="mb-32 grid text-center text-black lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-       
-          <ul className=" list-none m-0  max-w-[30ch] text-m ">
-            <li> Anton Mailänder</li>
-            <li className="opacity-50">anton@omarkitektur.no</li>
-            <li className="opacity-50">+46 70-960 45 39</li>
-          </ul>
-       
-          <ul className=" list-none m-0 max-w-[30ch] text-m">
-            <li> Ferdinand Aagenæs </li>
-            <li className="opacity-50">ferdinand@omarkitektur.no</li>
-            <li className="opacity-50">+47 90 55 25 60</li>
-          </ul>
+            <div
+                className="mb-32 grid text-center text-black lg:max-w-5xl lg:w-full lg:mb-0 grid-cols-2 gap-8  lg:grid-cols-4">
+                <ul className="list-none m-0  max-w-[30ch] text-md text-left">
+                    <li>Anton Mailänder</li>
+                    <li className="opacity-50"><a href="mailto:anton@omarkitektur.no">anton@omarkitektur.no</a></li>
+                    <li className="opacity-50"><a href="tel:+4670-9604539">+46 70-960 45 39</a></li>
+                </ul>
 
-          <ul className=" list-none m-0 max-w-[30ch] text-m">
-            <li> Jo Giæver Suul </li>
-            <li className="opacity-50">jo@omarkitektur.no</li>
-            <li className="opacity-50">+47 93 07 25 51</li>
-          </ul> 
+                <ul className=" list-none m-0 max-w-[30ch] text-md text-left">
+                    <li>Ferdinand Aagenæs</li>
+                    <li className="opacity-50"><a href="mailto:ferdinand@omarkitektur.no">ferdinand@omarkitektur.no</a>
+                    </li>
+                    <li className="opacity-50"><a href="tel:+4790552560">+47 90 55 25 60</a></li>
+                </ul>
 
-          <ul className=" list-none m-0 max-w-[30ch] text-m">
-            <li> Sivert Frost Støren </li>
-            <li className="opacity-50">sivert@omarkitektur.no</li>
-            <li className="opacity-50">+45 52 16 62 60</li>
-          </ul> 
-        
-      </div>
-    </main>
-  );
+                <ul className=" list-none m-0 max-w-[30ch] text-md text-left">
+                    <li>Jo Giæver Suul</li>
+                    <li className="opacity-50"><a href="mailto:jo@omarkitektur.no">jo@omarkitektur.no</a>
+                    </li>
+                    <li className="opacity-50"><a href="tel:+4793072551">+47 93 07 25 51</a></li>
+                </ul>
+
+                <ul className=" list-none m-0 max-w-[30ch] text-md text-left">
+                    <li>Sivert Frost Støren</li>
+                    <li className="opacity-50"><a href="mailto:sivert@omarkitektur.no">sivert@omarkitektur.no</a></li>
+                    <li className="opacity-50"><a href="tel:+4552166260">+45 52 16 62 60</a></li>
+                </ul>
+
+            </div>
+        </main>
+    );
 }
